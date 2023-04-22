a2b5f5fae32951047aa811bd0b58cadb632bc396
Arrrrrchi <esh6c4nm@gmail.com>
2023-04-22 10:04:20 +0900

    いいね機能

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import ArticleLike from './components/ArticleLike.vue';
+
+const app = createApp(App);
+app.component('article-like', ArticleLike);
+app.mount("#app");
