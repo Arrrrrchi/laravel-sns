2520fcf04fe6c10b45acc01070b332eb01de0ddb
Arrrrrchi <esh6c4nm@gmail.com>
2023-04-24 11:35:42 +0900

    デバッグバー

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,10 +16,10 @@
 </head>
 
 <body>
-
-  @yield('content')
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-  <!-- JQuery -->
+    
+    @yield('content')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- JQuery -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
