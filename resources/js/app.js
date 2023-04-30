72e16b932750fd6665a4bca818dde5bafb172356
Arrrrrchi <esh6c4nm@gmail.com>
2023-04-30 17:28:19 +0900

    フォロー機能

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,12 @@ import './bootstrap';
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import ArticleLike from './components/ArticleLike.vue';
 import ArticleTagsInput from './components/ArticleTagsInput.vue';
+import FollowButton from './components/FollowButton.vue';
 
 createApp({
     components: {
         ArticleLike,
         ArticleTagsInput,
+        FollowButton,
     }
 }).mount('#app');
